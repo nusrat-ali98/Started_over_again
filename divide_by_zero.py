36282ccb9f59d0ee2f447a0a1653c9cf035df3a5
Nusrat Ali <49001544+nusrat-ali98@users.noreply.github.com>
2025-10-08 16:06:36 +0200

    Add files via upload

--- divide_by_zero.py
+++ divide_by_zero.py
@@ -1,4 +1,16 @@
-try:
-    print(3/0)
-except ZeroDivisionError:
-    print("Division by zero is not possible")
+print("Enter two numbers for division")
+print("If you want to exit then press 'q'")
+
+while True:
+    first_number = input("Enter first number: ")
+
+    if first_number == 'q':
+        break
+    second_number = input("Enter second number: ")
+    try:
+        answer = int(first_number) / int(second_number)
+
+    except ZeroDivisionError:
+        print("You can't divide by zero")
+    else:
+        print(answer)
