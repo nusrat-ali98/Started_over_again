28843062c7d3a4907de53522b4f4f669379bba5c
Nusrat Ali <49001544+nusrat-ali98@users.noreply.github.com>
2025-10-08 16:29:47 +0200

    Add files via upload

--- file_not_found.py
+++ file_not_found.py
@@ -1,12 +1,16 @@
-filename = "aleix.txt"
 
-try:
-    with open(filename) as f:
-        contents = f.read()
-except FileNotFoundError:
-    msg = "The file " + filename + " was not found."
-    print(msg)
-else:
-    content = contents.split()
-    count = len(content)
-    print("The file " + filename + " has " + str(count) + " words.")
+def count_words(filename):
+    try:
+        with open(filename) as f:
+            contents = f.read()
+    except FileNotFoundError:
+        msg = "The file " + filename + " was not found."
+        print(msg)
+    else:
+        content = contents.split()
+        count = len(content)
+        print("The file " + filename + " has " + str(count) + " words.")
+
+
+filename = input("Enter file name: ")
+count_words(filename)
