9cbc23b9b67d30e88d537cb1bb862ee81bd38da9
Nusrat Ali <49001544+nusrat-ali98@users.noreply.github.com>
2025-10-20 12:42:47 +0200

    Add files via upload

--- series_creation.py
+++ series_creation.py
@@ -2,4 +2,16 @@ import pandas as pd
 
 
 student = pd.Series([80,48,99,12,10] , index=["Alice","Anwar","Nusrat","Bilal","Talib"])
-print(student)
+print(f"{student}\n")
+
+
+avg_marks = student.mean()
+print(f"average marks:\n{avg_marks}\n ")
+
+
+bouns_marks = student * 1.1
+print("Bouns marks:\n", bouns_marks)
+print("\n")
+
+data_obv_60 = student[student > 60]
+print("Student whose marks are above: \n", data_obv_60)
